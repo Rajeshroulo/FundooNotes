95b6234ee266fc4bd0caf5674d6e3352176ac3ba
Rajeshroulo <rajraval017@gmail.com>
2020-12-20 13:11:42 +0530

    Added code to user login to fundoonotes

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" dir="ltr">
 <head>
-<title>Login</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.css'); ?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?>">
+<title></title>
+<meta charset="utf-8">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> 
+   <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
 </head>
 <body>
 
